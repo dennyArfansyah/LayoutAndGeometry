ae81beb81b513dba147fad557272151246471fdc
dennyArfansyah <dennyarfansyah@gmail.com>
2023-08-18 10:52:22 +0700

    Initial Commit

---
+++ LayoutAndGeomerty/LayoutAndGeomertyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeomertyApp.swift
+//  LayoutAndGeomerty
+//
+//  Created by Denny Arfansyah on 18/08/23.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeomertyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
